57c6bc969f69ce07be230a4e84e660a4eb139848
dmt1337x <damianrycerz10@gmail.com>
2022-04-21 19:48:12 +0200

    404 design

--- src/user-app/src/app/pages/not-found.page.ts
+++ src/user-app/src/app/pages/not-found.page.ts
@@ -1,5 +1,13 @@
-import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  ViewEncapsulation,
+  ChangeDetectionStrategy,
+} from '@angular/core';
 
-@Component({ selector: 'app-not-found-page', templateUrl: './not-found.page.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
-export class NotFoundPage {
-}
+@Component({
+  selector: 'app-not-found-page',
+  templateUrl: './not-found.page.html',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NotFoundPage {}
