a96c7f724891c7335b9c707ce57c08bc21a1291b
dmt1337x <damianrycerz10@gmail.com>
2022-04-30 03:53:20 +0200

    firebase hosting

--- src/user-app/src/environments/environment.prod.ts
+++ src/user-app/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  firebase: {
+    apiKey: 'AIzaSyCwXQUAYS7xcd16obRB2jCRwRnmRuv7w0c',
+    authDomain: 'eventplanner-adb48.firebaseapp.com',
+    projectId: 'eventplanner-adb48',
+    storageBucket: 'eventplanner-adb48.appspot.com',
+    messagingSenderId: '379924452387',
+    appId: '1:379924452387:web:494f90a2d6ecdc1b6d4d96',
+    measurementId: 'G-1N7QV41RS0',
+  },
+  production: true,
 };
