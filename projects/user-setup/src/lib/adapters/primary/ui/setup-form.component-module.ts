390f0f7cf1cb58dd4d925bb125df887109274a40
dmt1337x <damianrycerz10@gmail.com>
2022-05-05 23:56:39 +0200

    add back arrow and edit design

--- projects/user-setup/src/lib/adapters/primary/ui/setup-form.component-module.ts
+++ projects/user-setup/src/lib/adapters/primary/ui/setup-form.component-module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SetupFormComponent } from './setup-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
-@NgModule({ imports: [CommonModule, ReactiveFormsModule],
-  	declarations: [SetupFormComponent],
-  	providers: [],
-  	exports: [SetupFormComponent] })
-export class SetupFormComponentModule {
-}
+@NgModule({
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
+  declarations: [SetupFormComponent],
+  providers: [],
+  exports: [SetupFormComponent],
+})
+export class SetupFormComponentModule {}
