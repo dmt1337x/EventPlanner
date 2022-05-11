9755a87a01ef99289f8dac5fa893aa1efa48f2cc
dmt1337x <damianrycerz10@gmail.com>
2022-05-11 22:11:49 +0200

    add login and register link

--- projects/user-authentication/src/lib/adapters/primary/ui/user-login.component-module.ts
+++ projects/user-authentication/src/lib/adapters/primary/ui/user-login.component-module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserLoginComponent } from './user-login.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   declarations: [UserLoginComponent],
   providers: [],
   exports: [UserLoginComponent],
--- projects/user-authentication/src/lib/adapters/primary/ui/user-registration.component-module.ts
+++ projects/user-authentication/src/lib/adapters/primary/ui/user-registration.component-module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { UserRegistrationComponent } from './user-registration.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   declarations: [UserRegistrationComponent],
   providers: [],
   exports: [UserRegistrationComponent],
