fa7de0a02b81fb6716e8da9ea2217ee029fceb74
damianrycerz10@gmail.com <damianrycerz10@gmail.com>
2022-05-22 21:30:49 +0200

    edit show participant in rooms

--- projects/rooms/src/lib/adapters/primary/ui/room-list.component-module.ts
+++ projects/rooms/src/lib/adapters/primary/ui/room-list.component-module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RoomListComponent } from './room-list.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { SeatInRoomsComponentModule } from './seat-in-rooms.component-module';
 
-@NgModule({ imports: [CommonModule, ReactiveFormsModule],
-  	declarations: [RoomListComponent],
-  	providers: [],
-  	exports: [RoomListComponent] })
-export class RoomListComponentModule {
-}
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    BsDropdownModule.forRoot(),
+    SeatInRoomsComponentModule,
+  ],
+  declarations: [RoomListComponent],
+  providers: [],
+  exports: [RoomListComponent],
+})
+export class RoomListComponentModule {}
